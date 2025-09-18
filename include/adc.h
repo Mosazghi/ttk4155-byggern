717bbea9d78a711d229aa83c69b48b8eaa416cef
Mosazghi <fest121@hotmail.com>
2025-09-18 07:18:40 +0200

    Fix ADC

--- include/adc.h
+++ include/adc.h
@@ -1,17 +1,17 @@
 #pragma once
 #include <avr/io.h>
-#include <stdio.h>
-#include <stdint.h>
 #include <avr/iom162.h>
+#include <stdint.h>
+#include <stdio.h>
 
-#define ADC_START 0x1000 
+#define ADC_START 0x1000
 
 typedef enum {
   ADC_CH0,
   ADC_CH1,
   ADC_CH2,
   ADC_CH3,
-} ADC_CHANNEL;
+} adc_channel_t;
 
 void adc_init();
-uint8_t adc_read(ADC_CHANNEL channel);
+uint8_t adc_read(adc_channel_t channel);
