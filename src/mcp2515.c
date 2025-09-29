749cca71261f60a462c436525bc2ae63f2c0bc28
Mosazghi <fest121@hotmail.com>
2025-09-29 11:45:10 +0200

    Init mcp driver

---
+++ src/mcp2515.c
@@ -0,0 +1,11 @@
+#include "mcp2515.h"
+
+#include <stdint.h>
+
+#include "spi.h"
+
+void mcp2515_init() {}
+
+uint8_t mcp2515_read(uint8_t address) {}
+
+void mcp2515_write(uint8_t address, uint8_t data) {}
