e34222d8979f7b0c443626af88e92d43f4be0420
Mosazghi <fest121@hotmail.com>
2025-09-29 14:23:42 +0200

    Make read function

--- src/mcp2515.c
+++ src/mcp2515.c
@@ -6,6 +6,12 @@
 
 void mcp2515_init() {}
 
-uint8_t mcp2515_read(uint8_t address) {}
+uint8_t mcp2515_read(uint8_t address) {
+  spi_select(SPI_MCP2515);
+  spi_transmit(MCP_READ);
+  spi_transmit(address);
+  uint8_t data = spi_receive();
+  spi_deselect(SPI_MCP2515);
+  return data;
 
-void mcp2515_write(uint8_t address, uint8_t data) {}
+  void mcp2515_write(uint8_t address, uint8_t data) {}
