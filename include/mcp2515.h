aec1e0b55bcd3cab7dcdb49e4f939dcf94881162
sjoulk <shd@live.no>
2025-10-06 07:55:37 +0200

    mcp2515_RTS supports 3 buffers. mcp2515_setmode included.

--- include/mcp2515.h
+++ include/mcp2515.h
@@ -131,3 +131,7 @@
 void mcp2515_init(void);
 uint8_t mcp2515_read(uint8_t address);
 void mcp2515_write(uint8_t address, uint8_t data);
+char mcp2515_read_status(char data);
+void mcp2515_RTS(int buffer);
+void mcp2515_bitmod(uint8_t address, uint8_t mask, uint8_t data);
+void mcp2515_setmode(uint8_t mode);
--- src/mcp2515.c
+++ src/mcp2515.c
@@ -39,9 +39,20 @@ char mcp2515_read_status(char data) {
 }
 
 // Request to send
-void mcp2515_RTS0(){
+void mcp2515_RTS(int buffer){       // Buffers 0-2
   spi_slave_select(SPI_MCP2515);
-  spi_transmit(MCP_RTS_TX0);
+  buffer = buffer % 3;
+  char data = MCP_RTS_TX0;
+  if (buffer == 0){
+    data = MCP_RTS_TX0;
+  }
+  else if (buffer == 1){
+    data = MCP_RTS_TX1;
+  }
+  else if (buffer == 2){
+    data = MCP_RTS_TX2;
+  }
+  spi_transmit(data);
   spi_slave_deselect();
 }
 
@@ -52,4 +63,9 @@ void mcp2515_bitmod(uint8_t address, uint8_t mask, uint8_t data) {
   spi_transmit(mask);               // 1 byte: bit=1: edit allowed
   spi_transmit(data);               // New bit values to be set
   spi_slave_deselect();
+}
+
+// modes: MODE_NORMAL, MODE_LOOPBACK
+void mcp2515_setmode(uint8_t mode){
+  mcp2515_bitmod(MCP_CANCTRL, 0x70, mode);
 }
