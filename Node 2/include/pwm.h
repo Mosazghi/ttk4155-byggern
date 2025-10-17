47c7847dfb2c551ee5bb13c869b336cf26446db1
sjoulk <shd@live.no>
2025-10-17 11:50:03 +0200

    Updated pwm_init & pwm_update_dutyCycle to use enums as arguments to select channel&pin.

--- Node 2/include/pwm.h
+++ Node 2/include/pwm.h
@@ -1,7 +1,32 @@
 #include "sam3xa.h"  // Header for SAM3X register definitions
 #include "sam.h"
-#define PB13 (1u << 13)
-#define PWM_CH1 1
+//#define PB13 (1u << 13)
+//#define PWM_CH1 1
 
-void pwm_init();
-void pwm_update_dutyCycle(int channel, int CDTY);
+#define F_PWM(hz) = (CHIP_FREQ_CPU_MAX / 64 * hz)
+//TODO: use F_PWM as pwm_init argument to set a given frequency??
+//TODO: #define dutyCycle() = ??
+
+enum PWM_CHANNEL{
+    PWM_CH0 = 0,
+    PWM_CH1,
+    PWM_CH2,
+    PWM_CH3,
+    PWM_CH4,
+    PWM_CH5,
+    PWM_CH6,
+    PWM_CH7
+};
+
+enum portb_pin{
+    PB12 = (1u << 12),  // High - D12
+    PB13 = (1u << 13),  // High - D21
+    PB14 = (1u << 14),  // High - D22
+    PB15 = (1u << 15),  // High - D23
+    PB16 = (1u << 16),  // Low  - D24
+    PB17 = (1u << 17),  // Low  - D25
+    PB18 = (1u << 18),  // Low  - D26
+    PB19 = (1u << 19)   // Low  - D27
+};
+void pwm_init(enum PWM_CHANNEL channel, enum portb_pin pin);
+void pwm_update_dutyCycle(enum PWM_CHANNEL channel, int CDTY);
--- Node 2/src/pwm.c
+++ Node 2/src/pwm.c
@@ -1,20 +1,20 @@
 #include "pwm.h"
 
-// Currently only defined for PB13 
-void pwm_init(){
+// PWM_init for PWM_channel, PortB_pin
+void pwm_init(enum PWM_CHANNEL channel, enum portb_pin pin){
 
     // 1. Enable peripheral clocks for PIOB & PWM
     PMC->PMC_PCER0 = (1 << ID_PIOB);
     PMC->PMC_PCER1 = (1 << (ID_PWM - 32));
 
     // 2. Enable peripheral mode B (disable PIO control)
-    PIOB->PIO_PDR = PB13;
+    PIOB->PIO_PDR = pin;
 
     // 3. Select perhipheral mode B (PWM)
-    PIOB->PIO_ABSR |= PB13;
+    PIOB->PIO_ABSR |= pin;
 
     // 4. Disable pull-up
-    PIOB->PIO_PUDR = PB13;
+    PIOB->PIO_PUDR = pin;
 
     // 5. Configure PWM clock: f_pwm = CLKA / period = 50Hz 
     // CLKA = MCK / (DIVA * 2^PREA), MCK = 84MHz, Duty Cycle = [0.9, 2.1]ms
@@ -22,17 +22,47 @@ void pwm_init(){
                                                         // DIVA(8) => divide by 8
 
     // 6. Configure PWM channel
-    PWM->PWM_CH_NUM[PWM_CH1].PWM_CMR = PWM_CMR_CPRE_CLKA;
-    PWM->PWM_CH_NUM[PWM_CH1].PWM_CPRD = 26250;           // Period: 50Hz = 20ms
-    PWM->PWM_CH_NUM[PWM_CH1].PWM_CDTY = 2625;            // Duty cycle: 2ms
+    PWM->PWM_CH_NUM[channel].PWM_CMR = PWM_CMR_CPRE_CLKA;
+    PWM->PWM_CH_NUM[channel].PWM_CPRD = 26250;           // Period: 50Hz = 20ms = 26250
+    PWM->PWM_CH_NUM[channel].PWM_CDTY = 2625;            // Duty cycle: 2ms = 2625
 
     // 7. Enable PWM channel 
-    PWM->PWM_ENA = (1 << PWM_CH1);
+    PWM->PWM_ENA = (1 << channel);
 }
 
 // Update duty cycle as a ratio of: (current_period / CDTY) for PWM channel
-void pwm_update_dutyCycle(int channel, int CDTY) {
+void pwm_update_dutyCycle(enum PWM_CHANNEL channel, int CDTY) {
 
     int period = PWM->PWM_CH_NUM[channel].PWM_CPRD;
     PWM->PWM_CH_NUM[channel].PWM_CDTYUPD = (period / CDTY);
 }
+
+// OLD: Currently only defined for PB13 
+// void pwm_init(){
+
+//     // 1. Enable peripheral clocks for PIOB & PWM
+//     PMC->PMC_PCER0 = (1 << ID_PIOB);
+//     PMC->PMC_PCER1 = (1 << (ID_PWM - 32));
+
+//     // 2. Enable peripheral mode B (disable PIO control)
+//     PIOB->PIO_PDR = PB13;
+
+//     // 3. Select perhipheral mode B (PWM)
+//     PIOB->PIO_ABSR |= PB13;
+
+//     // 4. Disable pull-up
+//     PIOB->PIO_PUDR = PB13;
+
+//     // 5. Configure PWM clock: f_pwm = CLKA / period = 50Hz 
+//     // CLKA = MCK / (DIVA * 2^PREA), MCK = 84MHz, Duty Cycle = [0.9, 2.1]ms
+//     PWM->PWM_CLK = PWM_CLK_PREA(3) | PWM_CLK_DIVA(8);   // PREA(3) => divide by 8
+//                                                         // DIVA(8) => divide by 8
+
+//     // 6. Configure PWM channel
+//     PWM->PWM_CH_NUM[PWM_CH1].PWM_CMR = PWM_CMR_CPRE_CLKA;
+//     PWM->PWM_CH_NUM[PWM_CH1].PWM_CPRD = 26250;           // Period: 50Hz = 20ms
+//     PWM->PWM_CH_NUM[PWM_CH1].PWM_CDTY = 2625;            // Duty cycle: 2ms
+
+//     // 7. Enable PWM channel 
+//     PWM->PWM_ENA = (1 << PWM_CH1);
+// }
